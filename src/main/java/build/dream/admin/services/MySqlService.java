3100636eeac63ab66a8c834af02e21013e096d6a
liuyandong <554030404@qq.com>
2018-10-10 19:58:32 +0800

    初始化项目！！

---
+++ src/main/java/build/dream/admin/services/MySqlService.java
@@ -0,0 +1,11 @@
+package build.dream.admin.services;
+
+import build.dream.common.api.ApiRest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MySqlService {
+    public ApiRest listConfigurations() {
+        return null;
+    }
+}
