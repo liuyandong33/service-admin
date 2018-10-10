f6644d4c624d67e8ea0d807b837f7257640f039f
liuyandong <554030404@qq.com>
2018-10-10 22:03:06 +0800

    初始化项目！！

--- src/main/java/build/dream/admin/controllers/MySqlController.java
+++ src/main/java/build/dream/admin/controllers/MySqlController.java
@@ -1,9 +1,23 @@
 package build.dream.admin.controllers;
 
+import build.dream.admin.services.MySqlService;
+import build.dream.common.utils.GsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/mySql")
 public class MySqlController {
+    @Autowired
+    private MySqlService mySqlService;
+
+    @RequestMapping(value = "/listConfigurations", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String listConfigurations() {
+        return GsonUtils.toJson(mySqlService.listConfigurations());
+    }
 }
--- src/main/java/build/dream/admin/services/MySqlService.java
+++ src/main/java/build/dream/admin/services/MySqlService.java
@@ -1,11 +1,43 @@
 package build.dream.admin.services;
 
+import build.dream.common.admin.domains.MySqlConfiguration;
 import build.dream.common.api.ApiRest;
+import build.dream.common.utils.DatabaseHelper;
+import build.dream.common.utils.SearchModel;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class MySqlService {
+    @Transactional(readOnly = true)
     public ApiRest listConfigurations() {
-        return null;
+        SearchModel searchModel = new SearchModel(true);
+        List<MySqlConfiguration> mySqlConfigurations = DatabaseHelper.findAll(MySqlConfiguration.class, searchModel);
+        Map<String, List<MySqlConfiguration>> mySqlConfigurationMap = new HashMap<String, List<MySqlConfiguration>>();
+        for (MySqlConfiguration mySqlConfiguration : mySqlConfigurations) {
+            String type = mySqlConfiguration.getType();
+            List<MySqlConfiguration> mySqlConfigurationList = mySqlConfigurationMap.get(type);
+            if (CollectionUtils.isEmpty(mySqlConfigurationList)) {
+                mySqlConfigurationList = new ArrayList<MySqlConfiguration>();
+                mySqlConfigurationMap.put(type, mySqlConfigurationList);
+            }
+            mySqlConfigurationList.add(mySqlConfiguration);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, List<MySqlConfiguration>> entry : mySqlConfigurationMap.entrySet()) {
+            stringBuilder.append("[").append(entry.getKey()).append("]").append("\n");
+            List<MySqlConfiguration> mySqlConfigurationList = entry.getValue();
+            for (MySqlConfiguration mySqlConfiguration : mySqlConfigurationList) {
+                stringBuilder.append(mySqlConfiguration.getName()).append("=").append(mySqlConfiguration.getValue()).append("\n");
+            }
+        }
+        return ApiRest.builder().data(stringBuilder.toString()).message("处理成功！").successful(true).build();
     }
 }
