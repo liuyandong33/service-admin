e6b7d74ae2f06f5558827162c88c250108ee40eb
liuyandong <554030404@qq.com>
2019-01-03 15:43:09 +0800

    错误修改！！

--- src/main/java/build/dream/admin/controllers/FileController.java
+++ src/main/java/build/dream/admin/controllers/FileController.java
@@ -4,9 +4,8 @@ import build.dream.admin.models.file.ListFilesModel;
 import build.dream.admin.services.FileService;
 import build.dream.common.annotations.ApiRestAction;
 import build.dream.common.utils.ApplicationHandler;
+import build.dream.common.utils.FileUtils;
 import build.dream.common.utils.LogUtils;
-import build.dream.common.utils.MimeMappingUtils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
 
 @Controller
 @RequestMapping(value = "/file")
@@ -28,69 +27,9 @@ public class FileController {
     }
 
     @RequestMapping(value = "/download", method = RequestMethod.GET)
-    public void download(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+    public void download(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         LogUtils.info(ApplicationHandler.getRequestHeaders().toString());
         File file = new File("C:\\Users\\liuyandong\\Desktop\\CentOS-7-x86_64-Minimal-1804.iso");
-        long fileLength = file.length();
-
-        String range = httpServletRequest.getHeader("Range");
-
-        String fileName = file.getName();
-        httpServletResponse.setContentType(MimeMappingUtils.obtainMimeTypeByFileName(fileName));
-        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=" + fileName);
-        httpServletResponse.setHeader("Accept-Ranges", "bytes");
-        httpServletResponse.setHeader("Content-Length", String.valueOf(fileLength));
-
-        long start = 0;
-        long end = 0;
-        if (StringUtils.isNotBlank(range)) {
-            httpServletResponse.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-            range = range.substring(6);
-            if (range.indexOf("-") == range.length() - 1) {
-                start = Long.parseLong(range);
-                end = fileLength - 1;
-            } else if (range.indexOf("-") == 0) {
-                long length = Long.parseLong(range.substring(1));
-                start = fileLength - length;
-                end = fileLength - 1;
-            } else {
-                String[] array = range.split("-");
-                start = Long.parseLong(array[0]);
-                end = Long.parseLong(array[1]);
-            }
-
-            httpServletResponse.setHeader("Content-Range", "bytes " + start + "-" + end + "/" + fileLength);
-        } else {
-            start = 0;
-            end = fileLength - 1;
-        }
-
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
-        try {
-            inputStream = new FileInputStream(file);
-            outputStream = httpServletResponse.getOutputStream();
-            inputStream.skip(start);
-            long count = (end - start + 1) / 1024;
-            int remainder = (int) ((end - start + 1) % 1024);
-            byte[] buffer = new byte[1024];
-            int length = 0;
-            for (int index = 0; index < count; index++) {
-                length = inputStream.read(buffer, 0, 1024);
-                outputStream.write(buffer, 0, length);
-                outputStream.flush();
-            }
-
-            if (remainder > 0) {
-                inputStream.read(buffer, 0, remainder);
-                outputStream.write(buffer, 0, remainder);
-                outputStream.flush();
-            }
-        } catch (Exception e) {
-
-        } finally {
-            outputStream.close();
-            inputStream.close();
-        }
+        FileUtils.download(httpServletRequest, httpServletResponse, file, true);
     }
 }
