49b12655feff65726d7bfd3a74d61330a3b91713
liuyandong <554030404@qq.com>
2020-03-09 14:33:04 +0800

    修改包名！！

--- src/main/java/build/dream/devops/jobs/HealthCheckJob.java
+++ src/main/java/build/dream/devops/jobs/HealthCheckJob.java
@@ -4,6 +4,7 @@ import build.dream.common.utils.LogUtils;
 import build.dream.common.utils.OkHttpUtils;
 import build.dream.devops.services.ServiceService;
 import okhttp3.Response;
+import org.apache.commons.collections.MapUtils;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -22,9 +23,15 @@ public class HealthCheckJob implements Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         List<Map<String, Object>> serviceNodes = serviceService.findAllServiceNodes();
         for (Map<String, Object> serviceNode : serviceNodes) {
+            Long serviceNodeId = MapUtils.getLongValue(serviceNode, "serviceNodeId");
+            Integer port = MapUtils.getIntValue(serviceNode, "port");
+            Integer status = MapUtils.getIntValue(serviceNode, "status");
+            Integer pid = MapUtils.getIntValue(serviceNode, "pid");
+            String healthCheckPath = MapUtils.getString(serviceNode, "healthCheckPath");
+            String ipAddress = MapUtils.getString(serviceNode, "ipAddress");
             Response response = null;
             try {
-                response = OkHttpUtils.doGetNative("http://localhost:8989/ping/ok");
+                response = OkHttpUtils.doGetNative("http://" + ipAddress + ":" + port + healthCheckPath);
                 if (response.isSuccessful()) {
                     continue;
                 }
