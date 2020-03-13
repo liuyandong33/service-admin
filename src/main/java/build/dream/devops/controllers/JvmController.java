c49b5b226d4d704abe40fcd887dab3fcbdb2e5b5
liuyandong <554030404@qq.com>
2020-03-13 16:55:36 +0800

    修改包名！！

---
+++ src/main/java/build/dream/devops/controllers/JvmController.java
@@ -0,0 +1,25 @@
+package build.dream.devops.controllers;
+
+import build.dream.common.annotations.PermitAll;
+import build.dream.common.utils.JacksonUtils;
+import build.dream.common.utils.JvmUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/jvm")
+@PermitAll
+public class JvmController {
+    @RequestMapping(value = "/getProcessId")
+    @ResponseBody
+    public String getProcessId() {
+        return String.valueOf(JvmUtils.getProcessId());
+    }
+
+    @RequestMapping(value = "/getVmArguments")
+    @ResponseBody
+    public String getVmArguments() {
+        return JacksonUtils.writeValueAsString(JvmUtils.getVmArguments());
+    }
+}
