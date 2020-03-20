5f79405b744b312d692d484261418b579257f2f2
liuyandong <554030404@qq.com>
2020-03-20 09:35:57 +0800

    修改包名！！

--- src/main/java/build/dream/devops/controllers/ProgramController.java
+++ src/main/java/build/dream/devops/controllers/ProgramController.java
@@ -1,9 +1,9 @@
 package build.dream.devops.controllers;
 
+import build.dream.common.annotations.ApiRestAction;
 import build.dream.devops.models.program.ListProgramsModel;
 import build.dream.devops.models.program.ListVersionsModel;
 import build.dream.devops.services.ProgramService;
-import build.dream.common.annotations.ApiRestAction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/program")
 public class ProgramController {
+    /**
+     * 查询程序列表
+     *
+     * @return
+     */
     @RequestMapping(value = "/listPrograms")
     @ResponseBody
     @ApiRestAction(modelClass = ListProgramsModel.class, serviceClass = ProgramService.class, serviceMethodName = "listPrograms", error = "查询程序列表失败")
@@ -18,6 +23,11 @@ public class ProgramController {
         return null;
     }
 
+    /**
+     * 查询程序版本
+     *
+     * @return
+     */
     @RequestMapping(value = "/listVersions")
     @ResponseBody
     @ApiRestAction(modelClass = ListVersionsModel.class, serviceClass = ProgramService.class, serviceMethodName = "listVersions", error = "查询程序版本列表失败")
