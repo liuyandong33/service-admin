a3eba464d5e4773fb0a83400b86e98b7289948df
liuyandong <554030404@qq.com>
2020-03-27 09:52:37 +0800

    修改包名！！

--- src/main/java/build/dream/devops/controllers/JvmController.java
+++ src/main/java/build/dream/devops/controllers/JvmController.java
@@ -5,6 +5,7 @@ import build.dream.common.annotations.PermitAll;
 import build.dream.common.api.ApiRest;
 import build.dream.common.utils.JacksonUtils;
 import build.dream.common.utils.JvmUtils;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/jvm")
 @PermitAll
 public class JvmController {
-    @RequestMapping(value = "/{methodName}")
+    @RequestMapping(value = "/{methodName}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     @ApiRestAction(error = "处理失败")
     public String call(@PathVariable String methodName) {
