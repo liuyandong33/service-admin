376dbb9665651f7887fedccd4468ac4ce74256cf
liuyandong <554030404@qq.com>
2020-03-30 15:10:37 +0800

    修改包名！！

--- src/main/java/build/dream/devops/aspects/LogAspect.java
+++ src/main/java/build/dream/devops/aspects/LogAspect.java
@@ -15,13 +15,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(value = Ordered.HIGHEST_PRECEDENCE)
 public class LogAspect {
-    @Around(value = "execution(public * build.dream.devops.services.LogService.listLogs(..))")
+    @Around(value = "execution(public * build.dream.devops.services.LogService.listLogs(build.dream.devops.models.log.ListLogsModel))")
     public Object listLogsAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         ListLogsModel listLogsModel = (ListLogsModel) proceedingJoinPoint.getArgs()[0];
         return doAround(proceedingJoinPoint, listLogsModel.getDeploymentEnvironment(), listLogsModel.getPartitionCode(), listLogsModel.getServiceName());
     }
 
-    @Around(value = "execution(public * build.dream.devops.services.LogService.clearLogs(..))")
+    @Around(value = "execution(public * build.dream.devops.services.LogService.clearLogs(build.dream.devops.models.log.ClearLogsModel))")
     public Object clearLogsAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         ClearLogsModel clearLogsModel = (ClearLogsModel) proceedingJoinPoint.getArgs()[0];
         return doAround(proceedingJoinPoint, clearLogsModel.getDeploymentEnvironment(), clearLogsModel.getPartitionCode(), clearLogsModel.getServiceName());
